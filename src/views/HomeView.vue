30f24321b53e9ced0decd08ebb341594b225439a
MNRMax <maximilian.shabatayev@gmail.com>
2023-08-13 00:57:43 -0400

    -

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { io } from "socket.io-client";
+import { onMounted } from "vue";
+
+const socket = io(`ws://127.0.0.1:5000/`)
+
+onMounted(async () => {
+    socket.on("connect", () => {
+        console.log("Successfully connected to server");
+        socket.emit("message", { message: "dsaudgay" })
+    });
+
+    socket.on("session_id", (data) => {
+        console.log(data)
+    })
+
+    socket.on("message", async (data) => {
+        console.log(data)
+    })
+
+})
+
+function ledOn() {
+    socket.emit("on")
+}
+
+function ledOff() {
+    socket.emit("off")
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <button @click="ledOn">on</button>
+    <button @click="ledOff">off</button>
 </template>
