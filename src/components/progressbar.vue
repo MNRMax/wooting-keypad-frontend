7acecd41cb9ee488176f39018c92bd5c2a83a27f
MNRMax <maximilian.shabatayev@gmail.com>
2023-08-15 23:30:48 -0400

    -

--- src/components/progressbar.vue
+++ src/components/progressbar.vue
@@ -8,18 +8,15 @@ function getStyle() {
 </script>
 
 <template>
-    <div id="loading mx-auto">
-        <div class="min-w-[5vw] h-[15vh] bg-yellow-400 absolute left-[25vw]"></div>
-        <div class="w-[5vw] bg-slate-700 absolute left-[25vw] z-10 origin-bottom-left" :style="getStyle()"></div>
+    <div>
+        <div class="translate-x-[50%]">
+            <div class="w-[80px] h-[15vh] bg-yellow-400 absolute left-0 translate-x-[-50%]"></div>
+            <div class="w-[80px] bg-slate-700 absolute left-0 z-10 origin-bottom-left translate-x-[-50%]" :style="getStyle()"></div>
+        </div>
+        <p class="text-white mt-[15vh] text-center w-[80px]">{{ props.progress/25 }} mm</p>
     </div>
 </template>
 
 <style scoped>
-
-@media (max-width: 1200px) {
-    .block-width{
-        width: 90vw;
-    }
-}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <script setup>
 import { io } from "socket.io-client";
 import { onMounted, ref } from "vue";
-import progressBar from "../components/progressbar.vue"
+import progressBar from "../components/progressbar.vue";
+import { ColorPicker } from 'vue-color-kit'
+import 'vue-color-kit/dist/vue-color-kit.css'
 
 const socket = io(`ws://127.0.0.1:5000/`)
 const color = ref("#000000")
 const q = ref(0)
+const w = ref(0)
+const connected = ref(false)
 
 onMounted(async () => {
     socket.on("connect", () => {
         console.log("Successfully connected to server");
-        socket.emit("message", { message: "dsaudgay" })
     });
 
     socket.on("session_id", (data) => {
@@ -21,9 +24,21 @@ onMounted(async () => {
         console.log(data)
     })
     socket.on("keys", async (data) => {
-        q.value = Math.round((data.q/1023) * 100)
+        q.value = Math.round((data.q/100) * 100)
+        w.value = Math.round((data.w/100) * 100)
+    })
+    socket.on("connected", async () => {
+        connected.value = true
+    })
+    socket.on("disconnected", async () => {
+        connected.value = false
     })
 })
+
+function handleColor(e) {
+    color.value = e.hex
+    socket.emit("on", {color: e.hex})
+}
 
 function ledOn() {
     socket.emit("on", {color: color.value})
@@ -32,15 +47,33 @@ function ledOn() {
 function ledOff() {
     socket.emit("off")
 }
+
+function cb() {
+    socket.emit("cb")
+}
+
+function ct() {
+    socket.emit("ct")
+}
+function d() {
+    socket.emit("d")
+}
 </script>
 
 <template>
-    <div class=" text-white w-[50vw] flex justify-around">
-        <button @click="ledOn">on</button>
-        <button @click="ledOff">off</button>
-    </div>
-    <input type="color" v-model="color">
-    <div class="">
-        <progressBar :progress="q"/>
+    <div v-if="connected">
+        <div class=" text-white w-[50vw] flex justify-around">
+            <button @click="ledOn">on</button>
+            <button @click="ledOff">off</button>
+            <button @click="ct">Calibrate Top</button>
+            <button @click="cb">Calibrate Bottom</button>
+            <button @click="d">Activate</button>
+        </div>
+        <ColorPicker :sucker-canvas="null" @changeColor="handleColor" class=" box-content"/>
+        <img src="../assets/pad.png" class="w-[240px] h-auto mx-auto">
+        <div class="flex justify-around w-[50vh] mx-auto mt-10">
+            <progressBar :progress="q" class="relative"/>
+            <progressBar :progress="w" class="relative"/>
+        </div>
     </div>
 </template>
